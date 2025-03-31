972b7f2382d121248cfbc30600df03da44f93e20
DanielGomesOtt <danielgomesott@gmail.com>
2025-03-30 22:52:44 -0300

    first commit

---
+++ BackendBuffMice/src/main/java/com/danielott/BackendBuffMice/BackendBuffMiceApplication.java
@@ -0,0 +1,13 @@
+package com.danielott.BackendBuffMice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBuffMiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBuffMiceApplication.class, args);
+	}
+
+}
---
+++ BackendBuffMice/src/test/java/com/danielott/BackendBuffMice/BackendBuffMiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielott.BackendBuffMice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBuffMiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
